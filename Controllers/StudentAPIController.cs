bd2e288cc70994f5b6828d3325fa37f6b154f095
RishiChaturvedi-7 <rishichaturvedi700@gmail.com>
2025-02-03 15:25:43 +0530

    Add project files.

---
+++ Controllers/StudentAPIController.cs
@@ -0,0 +1,37 @@
+﻿using ASPCoreWebAPICRUD.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPCoreWebAPICRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentAPIController : ControllerBase
+    {
+        private readonly AspcoreWebApicrudContext context;
+
+        public StudentAPIController(AspcoreWebApicrudContext _context)
+        {
+            context = _context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Student>>> GetStudents()
+        {
+            var data = await context.Students.ToListAsync();
+            return Ok(data);
+        }
+        [HttpGet("{id}")]
+        
+        public async Task<ActionResult<Student>> GetStudentById(int id)
+        {
+            var student = await context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return student;
+        }
+    }
+}
---
+++ Models/AspcoreWebApicrudContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPCoreWebAPICRUD.Models;
+
+public partial class AspcoreWebApicrudContext : DbContext
+{
+    public AspcoreWebApicrudContext()
+    {
+    }
+
+    public AspcoreWebApicrudContext(DbContextOptions<AspcoreWebApicrudContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Student__3214EC07921326EE");
+
+            entity.ToTable("Student");
+
+            entity.Property(e => e.FatherName)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Standard)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.StudentGender)
+                .HasMaxLength(10)
+                .IsUnicode(false);
+            entity.Property(e => e.StudentName)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
